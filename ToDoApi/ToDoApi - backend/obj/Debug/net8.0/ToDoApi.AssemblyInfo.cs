d3063a42f0450bb7f8b564d0e732d3b6cc88ae92
Brian <joseph.fickas@gmail.com>
2025-06-29 22:37:49 -0600

    Entregar examen

--- ToDoApi/ToDoApi - backend/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
+++ ToDoApi/ToDoApi - backend/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c1f26cc35f4844a139363cf3b58b0849030b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
